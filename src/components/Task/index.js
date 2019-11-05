3f7878d62b66aa0625f5d0ec8ee63c76d2a9f046
LeDinhPhuc <ledinhphucts98@gmail.com>
2019-11-05 23:36:35 +0700

    a

--- src/components/Task/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Task = ({ title }) => {
-  return <div className="app-task">task</div>;
-};
-export default Task;
--- src/components/content/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Layout } from "antd";
-import "./styles.css";
-const { Content } = Layout;
-
-function Contents({ children }) {
-  return <Content className="app-content">{children}</Content>;
-}
-export default Contents;
