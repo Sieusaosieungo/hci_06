03baf067b71930f40e0753cb1a471169fb3a9542
phamdaitai <phamdaitai9899@gmail.com>
2019-11-18 00:48:03 +0700

    update router list-task

---
+++ src/pages/Account/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import './style.css';
+import { Input, Button } from "antd";
+
+const Account = () => {
+  const [state, setState] = useState({
+  });
+
+  return (
+    <div className="account">
+      Hê
+    </div>
+  );
+};
+
+export default Account;
