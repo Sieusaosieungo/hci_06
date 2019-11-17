76b007c3de1960bdc11a374c0e5a14426707fe73
LeDinhPhuc <ledinhphucts98@gmail.com>
2019-11-18 02:18:53 +0700

    a

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Layout } from "antd";
 import "./App.css";
 import { Modal } from "antd";
 import { connect } from "react-redux";
+import { hideModal } from "./actions/index";
 
 function App({ modal, children, dispatch }) {
   return (
@@ -17,7 +18,7 @@ function App({ modal, children, dispatch }) {
           title={modal.title}
           visible={modal.visible}
           onOk={modal.onOk}
-          onCancel={modal.onCancel}
+          onCancel={() => dispatch(hideModal())}
           width="65vw"
         >
           {modal.Component}
