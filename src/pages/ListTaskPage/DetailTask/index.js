3c0b4676e8c83fefa4737e78a7244cf49d78a916
Sieusaosieungo <thinhhanh1@gmail.com>
2019-11-18 01:39:33 +0700

    finish detail task

---
+++ src/pages/ListTaskPage/DetailTask/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import './style.css';
+import { Icon } from 'antd';
+
+export default function index() {
+  return (
+    <div className='detail-task'>
+      <h2 className='task-detail-title'>Chi tiết công việc</h2>
+      <h3>
+        Tên công việc: <span className='detail-value'>Công việc tháng 11</span>
+      </h3>
+      <h3>Mục tiêu cần đạt được:</h3>
+      <div className='criteria'>
+        <p>
+          <Icon type='check' />
+          <span> Đảm bảo chất lượng 50 quy trình</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span> Đảm bảo chất lượng 50,000 sản phẩm đầu ra</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span> Đảm bảo chất lượng 1000kg nguyên liệu đầu vào</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span> Đảm bảo chất lượng 5000 bao bì</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span> Đảm bảo số lượng hàng trả về nhỏ hơn 100 sản phẩm</span>
+        </p>
+      </div>
+      <h3>
+        Người kiểm duyệt: <span className='detail-value'>Giám đốc</span>
+      </h3>
+      <h3>
+        Ngày giao: <span className='detail-value'>11-11-2019</span>
+      </h3>
+      <h3>
+        Deadline: <span className='detail-value'>11-12-2019</span>
+      </h3>
+      <h3>
+        Trạng thái: <span className='detail-value'>Đã hoàn thành</span>
+      </h3>
+      <h3>
+        Thời gian hoàn thành: <span className='detail-value'>09-12-2019</span>
+      </h3>
+      <h3>
+        File đính kèm: <a className='attach-file'>Task-description.docx</a>
+      </h3>
+      <h3>Tiêu chí đánh giá công việc: </h3>
+      <div className='criteria'>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value'> Đảm bảo chất lượng 50 quy trình</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value'>
+            {' '}
+            Đảm bảo chất lượng 50,000 sản phẩm đầu ra
+          </span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value'>
+            {' '}
+            Đảm bảo chất lượng 1000kg nguyên liệu đầu vào
+          </span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value'> Đảm bảo chất lượng 5000 bao bì</span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value'>
+            {' '}
+            Đảm bảo số lượng hàng trả về nhỏ hơn 100 sản phẩm
+          </span>
+        </p>
+        <p>
+          <Icon type='check' />
+          <span className='detail-value' n>
+            {' '}
+            Hoàn thành công việc đúng thời hạn
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
